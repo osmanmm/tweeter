fbcdad4be264b010c21aac79a23fb3c882f8db4f
osmanmm <mohamed.osman@outlook.com>
2021-09-21 00:36:35 +0000

    add composer-char-counter.js to the project to handle the exstra letter if it's more than 140

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  
+    const textArea = $(".tweet-text");
+  
+    textArea.on("input", function() {
+      let inputValue = $(this).val().length;
+      let counter = $(this).siblings(".tweet-extras").contents(".counter");
+    
+      counter.val(140 - inputValue);
+  
+      if (counter.val() < 0) {
+        counter.css("color", "red");
+      } else {
+        counter.css("color", "");
+      }
+    });
+  });
